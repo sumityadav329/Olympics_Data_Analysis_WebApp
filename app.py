97f9581279ba52780bc891735987d388fc6a2ccb
Sumit Yadav <sumityadav329@gmail.com>
2024-06-25 18:56:55 +0530

    updeated  app.py and helper.py #2

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import preprocessor,helper
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv('data\\athlete_events.csv')
 region_df = pd.read_csv('data\\noc_regions.csv')
@@ -24,5 +27,51 @@ if user_menu == 'Medal Tally':
 
     selected_year = st.sidebar.selectbox("Select Year", years)
     selected_country = st.sidebar.selectbox("Select Country", country)
-    medal_tally = helper.medal_tally(df)
-    st.dataframe(medal_tally)
+    
+    medal_tally = helper.fetch_medal_tally(df, selected_year, selected_country)
+    if selected_year == "Overall" and selected_country == "Overall":
+        st.title("Overall Tally")
+    if selected_year != "Overall" and selected_country == "Overall":
+        st.title("Medal Tally in " + str(selected_year) + " Olympics")
+    if selected_year == "Overall" and selected_country!= "Overall":
+        st.title(selected_country + " Overall Performance")
+    if selected_year != "Overall" and selected_country != "Overall":
+        st.title(selected_country + " Performance in " + str(selected_year) + " Olympics")
+
+    st.table(medal_tally)
+
+if user_menu == 'Overall Analysis':
+    editions = df['Year'].unique().shape[0] - 1
+    cities = df['City'].unique().shape[0]
+    sports = df['Sport'].unique().shape[0]
+    events = df['Event'].unique().shape[0]
+    athletes = df['Name'].unique().shape[0]
+    nations = df['region'].unique().shape[0]
+
+    st.title("Top Statistics")
+    col1,col2,col3 = st.columns(3)
+    with col1:
+        st.header("Editions")
+        st.title(editions)
+    with col2:
+        st.header("Hosts")
+        st.title(cities)
+    with col3:
+        st.header("Sports")
+        st.title(sports)
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.header("Events")
+        st.title(events)
+    with col2:
+        st.header("Nations")
+        st.title(nations)
+    with col3:
+        st.header("Athletes")
+        st.title(athletes)
+
+    nations_over_time = helper.data_over_time(df,'region')
+    fig = px.line(nations_over_time, x="Edition", y="region")
+    st.title("Participating Nations over the years")
+    st.plotly_chart(fig)
--- helper.py
+++ helper.py
@@ -1,4 +1,31 @@
 import numpy as np
+import pandas as pd
+def fetch_medal_tally(df, year, country):
+    medal_df = df.drop_duplicates(subset=['Team', 'NOC', 'Games',
+        'Year', 'City', 'Sport', 'Event', 'Medal'])
+    flag = 0
+    if year == "Overall" and country == "Overall":
+        temp_df = medal_df
+    if year == "Overall" and country != "Overall":
+        flag = 1
+        temp_df = medal_df[medal_df['region'] == country]
+    if year != "Overall" and country == "Overall":
+        temp_df = medal_df[medal_df['Year'] == int(year)]
+    if year != "Overall" and country != "Overall":
+        temp_df = medal_df[(medal_df['Year'] == int(year)) & (medal_df['region'] == country)]
+
+    if flag ==1:
+        x = temp_df.groupby('Year').sum()[['Gold','Silver','Bronze']].sort_values('Year').reset_index()
+    else:
+        x = temp_df.groupby('region').sum()[['Gold','Silver','Bronze']].sort_values('Gold', ascending = False).reset_index()
+    x['Total']  = x['Gold'] + x['Silver'] + x['Bronze']
+    x['Gold']   = x['Gold'].astype('int')
+    x['Silver'] = x['Silver'].astype('int')
+    x['Bronze'] = x['Bronze'].astype('int')
+    x['Total']  = x['Total'].astype('int')
+    
+    
+    return x
 
 def medal_tally(df):
     medal_tally = df.drop_duplicates(subset=['Team', 'NOC', 'Games',
@@ -25,3 +52,9 @@ def country_year_list(df):
     country.insert(0,'Overall')
 
     return years, country
+
+def data_over_time(df,col):
+
+    nations_over_time = df.drop_duplicates(['Year', col])['Year'].value_counts().reset_index().sort_values('index')
+    nations_over_time.rename(columns={'index': 'Edition', 'Year': col}, inplace=True)
+    return nations_over_time
