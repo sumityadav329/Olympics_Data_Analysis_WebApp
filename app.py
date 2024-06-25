49cee93a8218d68f1d106190083d75d587bf776e
Sumit Yadav <sumityadav329@gmail.com>
2024-06-25 15:55:13 +0530

    updated

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ region_df = pd.read_csv('data\\noc_regions.csv')
 
 df = preprocessor.preprocess(df, region_df)
 
+st.sidebar.title("Olympics Analysis")
+
 user_menu = st.sidebar.radio(
     "Select an Option",
     ("Medal Tally", "Overall Analysis", "Country-wise Analysis", "Athletes wise Analysis")
@@ -17,5 +19,10 @@ user_menu = st.sidebar.radio(
 #st.dataframe(df)
 
 if user_menu == 'Medal Tally':
+    st.sidebar.header("Medal Tally")
+    years, country = helper.country_year_list(df)
+
+    selected_year = st.sidebar.selectbox("Select Year", years)
+    selected_country = st.sidebar.selectbox("Select Country", country)
     medal_tally = helper.medal_tally(df)
     st.dataframe(medal_tally)
--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def medal_tally(df):
     medal_tally = df.drop_duplicates(subset=['Team', 'NOC', 'Games',
         'Year', 'City', 'Sport', 'Event', 'Medal'])
@@ -12,3 +14,14 @@ def medal_tally(df):
     medal_tally['Total'] = medal_tally['Total'].astype('int')
 
     return medal_tally
+
+def country_year_list(df):
+    years = df['Year'].unique().tolist()
+    years.sort()
+    years.insert(0,'Overall')
+
+    country = np.unique(df['region'].dropna().values).tolist()
+    country.sort()
+    country.insert(0,'Overall')
+
+    return years, country
